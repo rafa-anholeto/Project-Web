b06633e131a89be367c0d44eb8ae97bbef57b7d1
Rafael Anholeto <rafaanholeto3@gmail.com>
2021-11-09 16:03:50 -0300

    H2 database, test profile, JPA

--- CourseApplication/src/main/java/com/example/CourseApplication/entities/User.java
+++ CourseApplication/src/main/java/com/example/CourseApplication/entities/User.java
@@ -1,10 +1,17 @@
 package com.example.CourseApplication.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -23,6 +30,7 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    @Id
     public Long getId() {
         return id;
     }
@@ -75,4 +83,5 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
 }
