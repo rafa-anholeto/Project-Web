849c06f912d45bb1ca0033cb3b649d1c1b88cf70
Rafael Anholeto <rafaanholeto3@gmail.com>
2021-11-12 11:31:21 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- CourseApplication/src/main/java/com/example/CourseApplication/config/TestConfig.java
+++ CourseApplication/src/main/java/com/example/CourseApplication/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.example.CourseApplication.config;
 
-import com.example.CourseApplication.entities.Category;
-import com.example.CourseApplication.entities.Order;
-import com.example.CourseApplication.entities.Product;
-import com.example.CourseApplication.entities.User;
+import com.example.CourseApplication.entities.*;
 import com.example.CourseApplication.entities.enums.OrderStatus;
-import com.example.CourseApplication.repositories.CategoryRepository;
-import com.example.CourseApplication.repositories.OrderRepository;
-import com.example.CourseApplication.repositories.ProductRepository;
-import com.example.CourseApplication.repositories.UserRepository;
+import com.example.CourseApplication.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -34,6 +28,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -71,6 +68,13 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
     }
 }
--- CourseApplication/src/main/java/com/example/CourseApplication/entities/OrderItem.java
+++ CourseApplication/src/main/java/com/example/CourseApplication/entities/OrderItem.java
@@ -1,6 +1,7 @@
 package com.example.CourseApplication.entities;
 
 import com.example.CourseApplication.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -13,7 +14,7 @@ import java.util.Objects;
 public class OrderItem implements Serializable {
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
 
     private Integer quantity;
     private Double price;
@@ -28,6 +29,7 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    @JsonIgnore
     public Order getOrder(){
         return id.getOrder();
     }
